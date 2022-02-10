215e3ae7668e14c7e603aa9975eb4799620020cf
pedrofranca2502 <pedrofranca250204@gmail.com>
2022-02-10 20:45:17 -0300

    codigo completo

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# server
+Esse projeto tem a intencao de criar uma "Biblioteca", um site onde se possa vender livros de todos os generos, lugares, autores e muitos mais.
---
+++ livros.js
@@ -0,0 +1,62 @@
+[
+    {  
+       "titulo": "Jogador N1",
+       "genero": "Ficcao Cientifica",
+       "ano": "2017"
+    },
+   
+    {  
+       "titulo": "O Diario Perdido de Gravity Falls",
+       "genero": "Ficcao Cientifica",
+       "ano": "2017"
+    },
+   
+    {  
+       "titulo": "",
+       "genero": "",
+       "ano": "2017"
+    },
+   
+    {  
+       "titulo": "",
+       "genero": "",
+       "ano": "2017"
+    },
+   
+    {  
+       "titulo": "Jogador N1",
+       "genero": "Ficcao Cientifica",
+       "ano": "2017"
+    },
+   
+    {  
+      "titulo": "Jogador N1",
+      "genero": "Ficcao Cientifica",
+      "ano": "2017"
+   },
+   
+   {  
+      "titulo": "Jogador N1",
+      "genero": "Ficcao Cientifica",
+      "ano": "2017"
+   },
+   
+   {  
+      "titulo": "Jogador N1",
+      "genero": "Ficcao Cientifica",
+      "ano": "2017"
+   },
+   
+   {  
+      "titulo": "Jogador N1",
+      "genero": "Ficcao Cientifica",
+      "ano": "2017"
+   },
+   
+   {  
+      "titulo": "Jogador N1",
+      "genero": "Ficcao Cientifica",
+      "ano": "2017"
+   },
+   
+]
